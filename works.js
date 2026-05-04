70a93a0720466b70c94238c3d6db065d4942f1ae
shiroro98 <64116482+shiroro98@users.noreply.github.com>
2026-05-04 15:04:15 +0900

    Update works.js

--- works.js
+++ works.js
@@ -110,8 +110,8 @@ const illusts = [
   { url: "https://shiroro98.github.io/portfolio/images/2.png" },
   { url: "https://shiroro98.github.io/portfolio/images/TH73.png" },
   { url: "https://shiroro98.github.io/portfolio/images/iconu.png" },
-  { url: "https://shiroro98.github.io/portfolio/images/hapiramas.png" },
-  { url: "https://shiroro98.github.io/portfolio/images/shugo2x.jpg" },
+{ url: "https://shiroro98.github.io/portfolio/images/はぴらま四角.png" },
+{ url: "https://shiroro98.github.io/portfolio/images/集合2x.jpg" },
 ];
 
 const musics = [];
